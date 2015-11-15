ab069bbc4d0d07cc810b2ea6a350ccd9d4516710
romeric <roman_poya@yahoo.com>
2015-11-15 00:08:07 +0000

    Two solvers benchmarked and verified, the Linear elasticity with steps verified against Ruben's code and the nonlinear solver with Rogelio's code

--- Core/FiniteElements/Solvers/LinearSolver.py
+++ Core/FiniteElements/Solvers/LinearSolver.py
@@ -10,17 +10,14 @@ from Core.FiniteElements.Assembly import *
 from Core.FiniteElements.PostProcess import * 
 from Core.FiniteElements.ApplyDirichletBoundaryConditions import *
 
-def LinearSolver(Increment,MainData,K,F,M,NodalForces,Residual,ResidualNorm,nmesh,TotalDisp,Eulerx,
+def LinearSolver(Increment,MainData,K,F,NeumannForces,M,NodalForces,Residual,ResidualNorm,nmesh,TotalDisp,Eulerx,
 			columns_in,columns_out,AppliedDirichletInc):
 
 	# GET THE REDUCED ELEMENTAL MATRICES 
 	# K_b, F_b, _, _ = ApplyLinearDirichletBoundaryConditions(K,F,columns_in,columns_out,AppliedDirichletInc,MainData.Analysis,M)
-	K_b, F_b = ApplyLinearDirichletBoundaryConditions(K,Residual,columns_in,MainData.Analysis,M)[:2]
+	K_b, F_b = GetReducedMatrices(K,Residual,columns_in,M,MainData.Analysis)[:2]
 	
 	# SOLVE THE SYSTEM
-	# np.savetxt('/media/MATLAB/MeshingElasticity/F_now.dat',F_b)
-	# np.savetxt('/home/roman/Desktop/MeshingElasticity2/F_b.dat',F)
-	# np.savetxt('/home/roman/Desktop/MeshingElasticity2/K_b.dat',K.toarray())
 	t_solver=time()
 	if MainData.solve.type == 'direct':
 		# CHECK FOR THE CONDITION NUMBER OF THE SYSTEM
@@ -40,18 +37,36 @@ def LinearSolver(Increment,MainData,K,F,M,NodalForces,Residual,ResidualNorm,nmes
 	# UPDATE THE FIELDS
 	TotalDisp[:,:,Increment] += dU
 
+	print AppliedDirichletInc
+
 	# LINEARISED ELASTICITY WITH STRESS AND HESSIAN UPDATE
 	if MainData.Prestress:
 		# THE IF CONDITION IS TO AVOID ASSEMBLING FOR THE INCREMENT WHICH WE DON'T SOLVE FOR
-		if Increment <MainData.AssemblyParameters.LoadIncrements-1:
+		if Increment < MainData.AssemblyParameters.LoadIncrements-1:
 			# UPDATE THE GEOMETRY
 			Eulerx = nmesh.points + TotalDisp[:,:MainData.ndim,Increment]			
 			# RE-ASSEMBLE - COMPUTE INTERNAL TRACTION FORCES (BE CAREFUL ABOUT THE -1 INDEX IN HERE)
 			K, TractionForces = Assembly(MainData,nmesh,Eulerx,TotalDisp[:,MainData.nvar-1,Increment].reshape(TotalDisp.shape[0],1))[:2]
+			# print np.linalg.norm(TractionForces)
+
+			F = np.zeros_like(F)
+			Residual = np.zeros_like(Residual)
+			NodalForces = np.zeros_like(NodalForces)
+
+			# vmesh = copy.deepcopy(nmesh)
+			# vmesh.points = np.copy(Eulerx)
+			# F = ApplyDirichletBoundaryConditions(K,F,vmesh,MainData)[2]
+			
+
 			# FIND THE RESIDUAL
-			Residual[columns_in] = TractionForces[columns_in] - NodalForces[columns_in]
+			# Residual[columns_in] = TractionForces[columns_in] - NodalForces[columns_in]
+			for i in range(0,columns_out.shape[0]):
+				F = F - AppliedDirichletInc[i]*K.getcol(columns_out[i])
+			AppliedDirichletInc = np.zeros_like(AppliedDirichletInc)
+
+			PostProcess().MeshQualityMeasures(MainData,nmesh,TotalDisp[:,:,:Increment+1],show_plot=False)
 
 	print 'Load increment', Increment, 'for incrementally linearised elastic problem'
 
 	# RETURNING K IS NECESSARY
-	return TotalDisp, K
+	return TotalDisp, K, F, AppliedDirichletInc, Residual, NodalForces
