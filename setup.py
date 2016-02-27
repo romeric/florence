50ade9a3f9791f6e8f1aa736a51462e53a7eea96
romeric <roman_poya@yahoo.com>
2016-02-27 02:21:38 +0100

    put Jacobi build under florence setup

--- setup.py
+++ setup.py
@@ -7,18 +7,27 @@ from Cython.Build import cythonize
 
 # GET THE CURRENT WORKING DIRECTORY
 _pwd_ = os.path.dirname(os.path.realpath('__file__'))
+
 # BUILD TENSOR MODULE (NUMERIC AND LINALG)
 tensor_path = os.path.join(_pwd_,"Florence/Tensor/")
 print("Building Tensor module")
 p = subprocess.Popen('cd '+tensor_path+' && ./Makefile.sh', shell=True)
 p.wait()
-# BUILD OPENCASCADE FRONT-END
-print("Building OpenCascade front-end")
-occ_path = os.path.join(_pwd_,"Florence/BoundaryCondition/CurvilinearMeshing/PostMesh")
-p = subprocess.Popen('cd '+occ_path+' && ./Makefile.sh', shell=True)
-p.wait()
+
 # BUILD SALOME MESH READER
 print("Building mesh reader for salome")
 mesh_path = os.path.join(_pwd_,"Florence/MeshGeneration/")
 p = subprocess.Popen('cd '+mesh_path+' && ./Makefile.sh', shell=True)
+p.wait()
+
+# BUILD JACOBI MODULE
+print("Building Jacobi module")
+jacobi_path = os.path.join(_pwd_,"Florence/InterpolationFunctions/JacobiPolynomials/")
+p = subprocess.Popen('cd '+jacobi_path+' && ./Makefile.sh', shell=True)
+p.wait()
+
+# BUILD OPENCASCADE FRONT-END
+print("Building OpenCascade front-end")
+occ_path = os.path.join(_pwd_,"Florence/BoundaryCondition/CurvilinearMeshing/PostMesh")
+p = subprocess.Popen('cd '+occ_path+' && ./Makefile.sh', shell=True)
 p.wait()
