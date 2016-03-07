5defa945894b3870c47a0e89ccfbd925df1bf95b
romeric <roman_poya@yahoo.com>
2016-03-07 14:02:48 +0100

    maintenance. Remember that the scaled Jacobian for test cases is detF and not the Jacobian of isoparametric mapping

--- Florence/VariationalPrinciple/NearlyIncompressibleHuWashizu.py
+++ Florence/VariationalPrinciple/NearlyIncompressibleHuWashizu.py
@@ -1,31 +1,6 @@
 import numpy as np
-from Florence import Mesh
+from .VariationalPrinciple import MixedFormulations
 from Florence.FiniteElements.GetBasesAtInegrationPoints import GetBasesAtInegrationPoints
-
-class MixedFormulations():
-
-    def __init__(self,mesh,variables_order=(1,0)):
-        self.variables_order = variables_order
-        self.quadrature_rules = None
-        self.median = None
-
-
-class upFormulation():
-    pass
-
-
-class FiveFieldPenalty(MixedFormulations):
-
-    def __init__(self,mesh):
-
-        self.submeshes = []*4
-        # PREPARE SUBMESHES
-        if mesh.element_type == "tri":
-            self.submeshes[0].elements = mesh.elements[:,:3]
-            self.submeshes[0].nelem = mesh.elements.shape[0]
-            self.submeshes[0].points = mesh.points[:self.submeshes[0].elements.max()+1,:]
-            self.submeshes[0].element_type = "tri"
-
 
 
 class NearlyIncompressibleHuWashizu(MixedFormulations):
@@ -65,8 +40,3 @@ class NearlyIncompressibleHuWashizu(MixedFormulations):
 
     def GetLocalStiffness(self,condensate=True):
         pass
-
-
-            
-
-            
