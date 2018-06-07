eaf06b020c4ddb69362327864a7abf330e58ac80
romeric <roman_poya@yahoo.com>
2018-06-07 11:03:08 +0100

    Add more left out material models to the test suite

--- Florence/MaterialLibrary/IsotropicElectroMechanics_1.py
+++ Florence/MaterialLibrary/IsotropicElectroMechanics_1.py
@@ -39,9 +39,10 @@ class IsotropicElectroMechanics_1(Material):
 
         delta = StrainTensors['I']
         E = 1.0*ElectricFieldx
-        
+
         Ex = E.reshape(E.shape[0])
-        EE = np.dot(E,E.T)
+        EE = np.outer(E,E)
+        innerEE = np.dot(E,E.T)
 
         I = delta
         # C = lamb2*AijBkl(I,I) +mu2*(AikBjl(I,I)+AilBjk(I,I)) + varepsilon_1*(AijBkl(I,EE) + AijBkl(EE,I) - \
@@ -57,12 +58,12 @@ class IsotropicElectroMechanics_1(Material):
         C = lamb2*einsum("ij,kl",I,I) +mu2*(einsum("ik,jl",I,I)+einsum("il,jk",I,I)) +\
             varepsilon_1*(einsum("ij,kl",I,EE) + einsum("ij,kl",EE,I) - einsum("ik,jl",EE,I)- einsum("il,jk",I,EE) -\
             einsum("il,jl",I,EE)- einsum("ik,jl",I,EE) ) +\
-            varepsilon_1*(np.dot(E.T,E)[0,0])*(0.5*( einsum("ik,jl",I,I)+einsum("il,jk",I,I) )-0.5* einsum("ij,kl",I,I) )
+            varepsilon_1*(innerEE)*(0.5*( einsum("ik,jl",I,I)+einsum("il,jk",I,I) )-0.5* einsum("ij,kl",I,I) )
         C_Voigt = Voigt(C,1)
 
 
-        # Computing the hessian 
-        # Elasticity tensor (C - 4th order tensor) 
+        # Computing the hessian
+        # Elasticity tensor (C - 4th order tensor)
         # C[i,j,k,l] += lamb2*delta[i,j]*delta[k,l]+2.0*mu2*(delta[i,k]*delta[j,l]) #
 
         b = StrainTensors['b'][gcounter]
@@ -74,14 +75,14 @@ class IsotropicElectroMechanics_1(Material):
         # e[k,i,j] += 1.0*varepsilon_1*(E[i]*delta[j,k] + E[j]*delta[i,k] - delta[i,j]*E[k]) ##
 
         # Note that the actual piezoelectric tensor is symmetric wrt to the last two indices
-        # Actual tensor is: e[k,i,j] += 1.0*varepsilon_1*(E[i]*delta[j,k] + E[j]*delta[i,k] - delta[i,j]*E[k]) 
-        # We need to make its Voigt_form symmetric with respect to (j,k) instead of (i,j) 
+        # Actual tensor is: e[k,i,j] += 1.0*varepsilon_1*(E[i]*delta[j,k] + E[j]*delta[i,k] - delta[i,j]*E[k])
+        # We need to make its Voigt_form symmetric with respect to (j,k) instead of (i,j)
 
         # ORIGINAL
         # e_voigt = 1.0*varepsilon_1*(AijUk(I,Ex)+AikUj(I,Ex)-UiAjk(Ex,I)).T
 
         e_voigt = 1.0*varepsilon_1*( einsum('ij,k',I,Ex) + einsum('ik,j',I,Ex) - einsum('i,jk',Ex,I) ).T
-        e_voigt = Voigt(e_voigt,1)
+        e_voigt = Voigt(np.ascontiguousarray(e_voigt),1)
 
         # Dielectric Tensor (Permittivity - 2nd order)
         Permittivity = -varepsilon_1*delta ##
@@ -115,12 +116,12 @@ class IsotropicElectroMechanics_1(Material):
 
         be = np.dot(b,ElectricFieldx)
 
-        return 1.0*mu/J*b+(lamb*(J-1.0)-mu)*I + varepsilon_1*(np.dot(E,E.T)-0.5*np.dot(E.T,E)*I) ## 
+        return 1.0*mu/J*b+(lamb*(J-1.0)-mu)*I + varepsilon_1*(np.dot(E,E.T)-0.5*np.dot(E.T,E)*I) ##
         # return 1.0*mu/J*b+(lamb*(J-1.0)-mu)*I - (2.0*varepsilon_1/J)*np.dot(be,be.T)
 
 
     def ElectricDisplacementx(self, StrainTensors, ElectricFieldx, elem=0, gcounter=0):
-        
+
         varepsilon_1 = self.eps_1
         return varepsilon_1*ElectricFieldx[:,None] ##
 
@@ -128,4 +129,4 @@ class IsotropicElectroMechanics_1(Material):
         # b = StrainTensors['b'][gcounter]
         # bb =  np.dot(b,b)
         # return (2.0*varepsilon_1/StrainTensors.J)*np.dot(bb,ElectricFieldx).reshape(StrainTensors.b.shape[0],1)
-        
+
