45caeeeef59f5c1b9566ade284d243a5fc4d93f3
romeric <roman_poya@yahoo.com>
2018-06-23 03:29:52 +0100

    Low level explicit solver now directly dispatches matmul calls to BLAS defeating Fastor functions

--- Florence/FiniteElements/Assembly/_Assembly_/_LowLevelAssemblyExplicit_DF_DPF_.h
+++ Florence/FiniteElements/Assembly/_Assembly_/_LowLevelAssemblyExplicit_DF_DPF_.h
@@ -24,16 +24,111 @@
 
 // Kinematics
 //
+
+#ifdef __AVX__
+
 template<int ndim, typename std::enable_if<ndim==2,bool>::type = 0>
-FASTOR_INLINE void KinematicMeasures__(Real *MaterialGradient, Real *current_sp, Real *current_F, Real &detJ,
-    const Real *current_Jm, Real AllGauss_, const Real *LagrangeElemCoords_, const Real *EulerElemCoords_,
-    int ngauss, int nodeperelem, int update)  {
+FASTOR_INLINE void KinematicMeasures__(
+    Real* FASTOR_RESTRICT MaterialGradient,
+    Real* FASTOR_RESTRICT current_sp,
+    Real* FASTOR_RESTRICT current_F,
+    Real &detJ,
+    const Real* FASTOR_RESTRICT current_Jm,
+    Real AllGauss_,
+    const Real* FASTOR_RESTRICT LagrangeElemCoords_,
+    const Real *FASTOR_RESTRICT EulerElemCoords_,
+    int nodeperelem,
+    int update)  {
 
     Real FASTOR_ALIGN ParentGradientX[ndim*ndim];
     Real FASTOR_ALIGN invParentGradientX[ndim*ndim];
     Real FASTOR_ALIGN ParentGradientx[ndim*ndim];
     Real FASTOR_ALIGN invParentGradientx[ndim*ndim];
     Real FASTOR_ALIGN current_Ft[ndim*ndim];
+
+    // USING A STL BASED FILLER REMOVES THE ANNOYING BUG
+    std::fill_n(ParentGradientX,ndim*ndim,0.);
+    std::fill_n(ParentGradientx,ndim*ndim,0.);
+    std::fill_n(current_Ft,ndim*ndim,0.);
+
+    _matmul_2k2(nodeperelem,current_Jm,LagrangeElemCoords_,ParentGradientX);
+    _matmul_2k2(nodeperelem,current_Jm,EulerElemCoords_,ParentGradientx);
+
+    inv2x2(ParentGradientX,invParentGradientX);
+    const Real detx = invdet2x2(ParentGradientx,invParentGradientx);
+
+    detJ = AllGauss_*fabs(detx);
+
+    _matmul_22k(nodeperelem,invParentGradientX,current_Jm,MaterialGradient);
+    _matmul_22k(nodeperelem,invParentGradientx,current_Jm,current_sp);
+
+    // Compute deformation gradient F
+    _matmul_2k2(nodeperelem,MaterialGradient,EulerElemCoords_,current_Ft);
+    Fastor::_transpose<Real,ndim,ndim>(current_Ft,current_F);
+}
+
+template<int ndim, typename std::enable_if<ndim==3,bool>::type = 0>
+FASTOR_INLINE void KinematicMeasures__(
+    Real* FASTOR_RESTRICT MaterialGradient,
+    Real* FASTOR_RESTRICT current_sp,
+    Real* FASTOR_RESTRICT current_F,
+    Real &detJ,
+    const Real* FASTOR_RESTRICT current_Jm,
+    Real AllGauss_,
+    const Real* FASTOR_RESTRICT LagrangeElemCoords_,
+    const Real *FASTOR_RESTRICT EulerElemCoords_,
+    int nodeperelem,
+    int update)  {
+
+    Real FASTOR_ALIGN ParentGradientX[ndim*ndim];
+    Real FASTOR_ALIGN invParentGradientX[ndim*ndim];
+    Real FASTOR_ALIGN ParentGradientx[ndim*ndim];
+    Real FASTOR_ALIGN invParentGradientx[ndim*ndim];
+    Real FASTOR_ALIGN current_Ft[ndim*ndim];
+
+    // USING A STL BASED FILLER REMOVES THE ANNOYING BUG
+    std::fill_n(ParentGradientX,ndim*ndim,0.);
+    std::fill_n(ParentGradientx,ndim*ndim,0.);
+    std::fill_n(current_Ft,ndim*ndim,0.);
+
+    _matmul_3k3(nodeperelem,current_Jm,LagrangeElemCoords_,ParentGradientX);
+    _matmul_3k3(nodeperelem,current_Jm,EulerElemCoords_,ParentGradientx);
+
+    inv3x3(ParentGradientX,invParentGradientX);
+    const Real detx = invdet3x3(ParentGradientx,invParentGradientx);
+
+    detJ = AllGauss_*std::abs(detx);
+
+    _matmul_33k(nodeperelem,invParentGradientX,current_Jm,MaterialGradient);
+    _matmul_33k(nodeperelem,invParentGradientx,current_Jm,current_sp);
+
+    // Compute deformation gradient F
+    _matmul_3k3(nodeperelem,MaterialGradient,EulerElemCoords_,current_Ft);
+    Fastor::_transpose<Real,ndim,ndim>(current_Ft,current_F);
+
+}
+
+
+
+#else
+
+
+
+template<int ndim, typename std::enable_if<ndim==2,bool>::type = 0>
+FASTOR_INLINE void KinematicMeasures__(Real *MaterialGradient, Real *current_sp, Real *current_F, Real &detJ,
+    const Real *current_Jm, Real AllGauss_, const Real *LagrangeElemCoords_, const Real *EulerElemCoords_,
+    int nodeperelem, int update)  {
+
+    Real FASTOR_ALIGN ParentGradientX[ndim*ndim];
+    Real FASTOR_ALIGN invParentGradientX[ndim*ndim];
+    Real FASTOR_ALIGN ParentGradientx[ndim*ndim];
+    Real FASTOR_ALIGN invParentGradientx[ndim*ndim];
+    Real FASTOR_ALIGN current_Ft[ndim*ndim];
+
+    // USING A STL BASED FILLER REMOVES THE ANNOYING BUG
+    std::fill_n(ParentGradientX,ndim*ndim,0.);
+    std::fill_n(ParentGradientx,ndim*ndim,0.);
+    std::fill_n(current_Ft,ndim*ndim,0.);
 
     _matmul_(ndim,ndim,nodeperelem,current_Jm,LagrangeElemCoords_,ParentGradientX);
     _matmul_(ndim,ndim,nodeperelem,current_Jm,EulerElemCoords_,ParentGradientx);
@@ -54,13 +149,18 @@ FASTOR_INLINE void KinematicMeasures__(Real *MaterialGradient, Real *current_sp,
 template<int ndim, typename std::enable_if<ndim==3,bool>::type = 0>
 FASTOR_INLINE void KinematicMeasures__(Real *MaterialGradient, Real *current_sp, Real *current_F, Real &detJ,
     const Real *current_Jm, Real AllGauss_, const Real *LagrangeElemCoords_, const Real *EulerElemCoords_,
-    int ngauss, int nodeperelem, int update)  {
+    int nodeperelem, int update)  {
 
     Real FASTOR_ALIGN ParentGradientX[ndim*ndim];
     Real FASTOR_ALIGN invParentGradientX[ndim*ndim];
     Real FASTOR_ALIGN ParentGradientx[ndim*ndim];
     Real FASTOR_ALIGN invParentGradientx[ndim*ndim];
     Real FASTOR_ALIGN current_Ft[ndim*ndim];
+
+    // USING A STL BASED FILLER REMOVES THE ANNOYING BUG
+    std::fill_n(ParentGradientX,ndim*ndim,0.);
+    std::fill_n(ParentGradientx,ndim*ndim,0.);
+    std::fill_n(current_Ft,ndim*ndim,0.);
 
     _matmul_(ndim,ndim,nodeperelem,current_Jm,LagrangeElemCoords_,ParentGradientX);
     _matmul_(ndim,ndim,nodeperelem,current_Jm,EulerElemCoords_,ParentGradientx);
@@ -77,6 +177,8 @@ FASTOR_INLINE void KinematicMeasures__(Real *MaterialGradient, Real *current_sp,
     _matmul_(ndim,ndim,nodeperelem,MaterialGradient,EulerElemCoords_,current_Ft);
     Fastor::_transpose<Real,ndim,ndim>(current_Ft,current_F);
 }
+
+#endif
 // // //
 
 
@@ -219,7 +321,6 @@ void _GlobalAssemblyExplicit_DF_DPF_<2>(const Real *points,
                                     AllGauss[g],
                                     LagrangeElemCoords,
                                     EulerElemCoords,
-                                    ngauss,
                                     nodeperelem,
                                     requires_geometry_update
                                     );
@@ -440,7 +541,6 @@ void _GlobalAssemblyExplicit_DF_DPF_<3>(const Real *points,
                                     AllGauss[g],
                                     LagrangeElemCoords,
                                     EulerElemCoords,
-                                    ngauss,
                                     nodeperelem,
                                     requires_geometry_update
                                     );
@@ -985,272 +1085,3 @@ void _GlobalAssemblyExplicit_DF_DPF_(const Real *points,
 
 
 #endif // _LOWLEVELASSEMBLYDPF__H
-
-
-
-
-
-
-
-
-// TWO GENERATION OLD - ORIGINAL NON-UNROLLED VERSION
-
-
-// #ifndef _LOWLEVELASSEMBLYDPF__H
-// #define _LOWLEVELASSEMBLYDPF__H
-
-// #include "assembly_helper.h"
-// #include "_TractionDF_.h"
-// #include "_TractionDPF_.h"
-
-// #include "_NeoHookean_.h"
-// #include "_MooneyRivlin_.h"
-// #include "_ExplicitMooneyRivlin_.h"
-// #include "_IsotropicElectroMechanics_101_.h"
-// #include "_IsotropicElectroMechanics_108_.h"
-
-// void _GlobalAssemblyExplicit_DF_DPF_(const Real *points,
-//                         const UInteger* elements,
-//                         const Real* Eulerx,
-//                         const Real* Eulerp,
-//                         const Real* bases,
-//                         const Real* Jm,
-//                         const Real* AllGauss,
-//                         Integer ndim,
-//                         Integer nvar,
-//                         Integer ngauss,
-//                         Integer nelem,
-//                         Integer nodeperelem,
-//                         Integer nnode,
-//                         Integer H_VoigtSize,
-//                         Integer requires_geometry_update,
-//                         Real *T,
-//                         Integer is_dynamic,
-//                         Integer* local_rows_mass,
-//                         Integer* local_cols_mass,
-//                         int *I_mass,
-//                         int *J_mass,
-//                         Real *V_mass,
-//                         Real rho,
-//                         Real mu,
-//                         Real mu1,
-//                         Real mu2,
-//                         Real mu3,
-//                         Real mue,
-//                         Real lamb,
-//                         Real eps_1,
-//                         Real eps_2,
-//                         Real eps_3,
-//                         Real eps_e,
-//                         const Real *anisotropic_orientations,
-//                         int material_number,
-//                         int formulation_number
-//                         ) {
-
-//     Integer ndof = nvar*nodeperelem;
-//     Integer local_capacity = ndof*ndof;
-
-
-//     Real *LagrangeElemCoords        = allocate<Real>(nodeperelem*ndim);
-//     Real *EulerElemCoords           = allocate<Real>(nodeperelem*ndim);
-//     Real *ElectricPotentialElem     = allocate<Real>(nodeperelem);
-
-//     Real *F                         = allocate<Real>(ngauss*ndim*ndim);
-//     Real *SpatialGradient           = allocate<Real>(ngauss*nodeperelem*ndim);
-//     Real *detJ                      = allocate<Real>(ngauss);
-
-//     Real *ElectricFieldx            = allocate<Real>(ngauss*ndim);
-
-//     Real *D                         = allocate<Real>(ngauss*ndim);
-//     Real *stress                    = allocate<Real>(ngauss*ndim*ndim);
-//     Real *hessian                   = allocate<Real>(ngauss*H_VoigtSize*H_VoigtSize);
-
-//     Real *traction                  = allocate<Real>(ndof);
-//     Real *mass                      = allocate<Real>(local_capacity);
-
-
-//     auto mat_obj0 = _NeoHookean_<Real>(mu,lamb);
-//     auto mat_obj1 = _MooneyRivlin_<Real>(mu1,mu2,lamb);
-//     auto mat_obj2 = _ExplicitMooneyRivlin_<Real>(mu1,mu2,lamb);
-//     auto mat_obj3 = _IsotropicElectroMechanics_101_<Real>(mu,lamb,eps_1);
-//     auto mat_obj4 = _IsotropicElectroMechanics_108_<Real>(mu1,mu2,lamb,eps_2);
-
-
-//     // LOOP OVER ELEMETNS
-//     for (Integer elem=0; elem < nelem; ++elem) {
-
-//         // GET THE FIELDS AT THE ELEMENT LEVEL
-//         for (Integer i=0; i<nodeperelem; ++i) {
-//             const Integer inode = elements[elem*nodeperelem+i];
-//             for (Integer j=0; j<ndim; ++j) {
-//                 LagrangeElemCoords[i*ndim+j] = points[inode*ndim+j];
-//                 EulerElemCoords[i*ndim+j] = Eulerx[inode*ndim+j];
-//             }
-//             ElectricPotentialElem[i] = Eulerp[inode];
-//         }
-
-//         // COMPUTE KINEMATIC MEASURES
-//         std::fill(F,F+ngauss*ndim*ndim,0.);
-//         std::fill(SpatialGradient,SpatialGradient+ngauss*nodeperelem*ndim,0.);
-//         std::fill(detJ,detJ+ngauss,0.);
-//         KinematicMeasures(  SpatialGradient,
-//                             F,
-//                             detJ,
-//                             Jm,
-//                             AllGauss,
-//                             LagrangeElemCoords,
-//                             EulerElemCoords,
-//                             ngauss,
-//                             ndim,
-//                             nodeperelem,
-//                             requires_geometry_update
-//                             );
-
-//         // COMPUTE ELECTRIC FIELD
-//         for (Integer i=0; i<ngauss; ++i) {
-//             for (Integer k=0; k<ndim; ++k) {
-//                 Real iE = 0;
-//                 for (Integer j=0; j<nodeperelem; ++j) {
-//                     iE += SpatialGradient[i*nodeperelem*ndim+j*ndim+k]*ElectricPotentialElem[j];
-//                 }
-//                 ElectricFieldx[i*ndim+k] = -iE;
-//             }
-//         }
-
-//         // COMPUTE KINETIC MEASURES
-//         if (material_number==0) {
-//             mat_obj0.KineticMeasures(stress, hessian, ndim, ngauss, F);
-//         }
-//         else if (material_number==1) {
-//             mat_obj1.KineticMeasures(stress, hessian, ndim, ngauss, F);
-//         }
-//         else if (material_number==2) {
-//             mat_obj2.KineticMeasures(stress, ndim, ngauss, F);
-//         }
-//         else if (material_number==3) {
-//             mat_obj3.KineticMeasures(D, stress, hessian, ndim, ngauss, F, ElectricFieldx);
-//         }
-//         else if (material_number==4) {
-//             mat_obj4.KineticMeasures(D, stress, hessian, ndim, ngauss, F, ElectricFieldx);
-//         }
-
-//         // COMPUTE CONSTITUTIVE STIFFNESS AND TRACTION
-//         std::fill(traction,traction+ndof,0.);
-//         if (formulation_number==1) {
-//             _TractionDPF_Filler_(
-//                 traction,
-//                 SpatialGradient,
-//                 D,
-//                 stress,
-//                 detJ,
-//                 ngauss,
-//                 nodeperelem,
-//                 ndim,
-//                 nvar,
-//                 H_VoigtSize,
-//                 requires_geometry_update);
-//         }
-//         else {
-//             _TractionDF_Filler_(
-//                 traction,
-//                 SpatialGradient,
-//                 stress,
-//                 detJ,
-//                 ngauss,
-//                 nodeperelem,
-//                 ndim,
-//                 nvar,
-//                 H_VoigtSize,
-//                 requires_geometry_update);
-//         }
-
-//         // ASSEMBLE TRACTIONS
-//         {
-//             for (Integer i = 0; i<nodeperelem; ++i) {
-//                 UInteger T_idx = elements[elem*nodeperelem+i]*nvar;
-//                 for (Integer iterator = 0; iterator < nvar; ++iterator) {
-//                     T[T_idx+iterator] += traction[i*nvar+iterator];
-//                 }
-//             }
-//         }
-
-//     }
-
-//     // ASSEMBLE MASS
-//     if (is_dynamic) {
-//         // This is performed only once as mass integrand is Lagrangian
-//         // hence not mixing this with stiffness and mass integrand is beneficial
-//         for (Integer elem=0 ; elem<nelem; ++elem) {
-
-//             // GET THE FIELDS AT THE ELEMENT LEVEL
-//             for (Integer i=0; i<nodeperelem; ++i) {
-//                 const Integer inode = elements[elem*nodeperelem+i];
-//                 for (Integer j=0; j<ndim; ++j) {
-//                     LagrangeElemCoords[i*ndim+j] = points[inode*ndim+j];
-//                     EulerElemCoords[i*ndim+j] = Eulerx[inode*ndim+j];
-//                 }
-//             }
-
-//             // COMPUTE KINEMATIC MEASURES
-//             std::fill(F,F+ngauss*ndim*ndim,0.);
-//             std::fill(SpatialGradient,SpatialGradient+ngauss*nodeperelem*ndim,0.);
-//             std::fill(detJ,detJ+ngauss,0.);
-//             KinematicMeasures(  SpatialGradient,
-//                                 F,
-//                                 detJ,
-//                                 Jm,
-//                                 AllGauss,
-//                                 LagrangeElemCoords,
-//                                 EulerElemCoords,
-//                                 ngauss,
-//                                 ndim,
-//                                 nodeperelem,
-//                                 0
-//                                 );
-
-//             std::fill(mass,mass+local_capacity,0);
-
-//             // Call MassIntegrand
-//             _MassIntegrand_Filler_( mass,
-//                                     bases,
-//                                     detJ,
-//                                     ngauss,
-//                                     nodeperelem,
-//                                     ndim,
-//                                     nvar,
-//                                     rho);
-
-//             // Fill IJV
-//             fill_triplet(   local_rows_mass,
-//                             local_cols_mass,
-//                             mass,
-//                             I_mass,
-//                             J_mass,
-//                             V_mass,
-//                             elem,
-//                             nvar,
-//                             nodeperelem,
-//                             elements,
-//                             local_capacity,
-//                             local_capacity);
-//         }
-//     }
-
-//     deallocate(LagrangeElemCoords);
-//     deallocate(EulerElemCoords);
-//     deallocate(ElectricPotentialElem);
-
-//     deallocate(F);
-//     deallocate(SpatialGradient);
-//     deallocate(detJ);
-//     deallocate(ElectricFieldx);
-//     deallocate(D);
-//     deallocate(stress);
-//     deallocate(hessian);
-//     deallocate(traction);
-//     deallocate(mass);
-
-// }
-
-
-// #endif // _LOWLEVELASSEMBLYDPF__H
